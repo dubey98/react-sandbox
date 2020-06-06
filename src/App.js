4908ae9ae2456fc8f26ffed73f5035867cbc2240
shiv dubey <dubeyshivprasad98@gmail.com>
2020-06-06 20:39:01 +0530

    delete and add new todos complete

--- src/App.js
+++ src/App.js
@@ -5,21 +5,41 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      todos: [],
+      todoTitle: "",
+    };
+  }
+
+  handleForm(e) {
+    e.preventDefault();
+    const title = this.state.todoTitle;
+    this.setState({
       todos: [
+        ...this.state.todos,
         {
-          title: "",
+          title: title,
           done: false,
         },
       ],
-    };
+      todoTitle: "",
+    });
+    console.log(this.state.todos);
   }
-  handleForm(e) {
-    e.preventDefault();
-    console.log(e);
-  }
+
   takeinput(e) {
-    console.log(e.target.value);
+    this.setState({
+      todoTitle: e.target.value,
+    });
   }
+
+  deleteTask(e, index) {
+    const todos = this.state.todos.slice();
+    todos.splice(index, 1);
+    this.setState({
+      todos: todos,
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,11 +51,27 @@ class App extends React.Component {
             name="newTodo"
             placeholder="new Todo"
             onChange={(e) => this.takeinput(e)}
+            value={this.state.todoTitle}
           />
           <button type="submit" onClick={(e) => this.handleForm(e)}>
             create
           </button>
         </form>
+        <hr />
+        <div>
+          {this.state.todos.map((todo, index) => {
+            return (
+              <div key={todo.title} className="todo">
+                <h3>{todo.title}</h3>
+                <div>
+                  <button onClick={(e, index) => this.deleteTask(e, index)}>
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
